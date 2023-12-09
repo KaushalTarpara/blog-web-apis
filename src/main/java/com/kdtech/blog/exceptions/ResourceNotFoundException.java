c0c9c2d0689e206dd878fb07b29f13beb70d73a2
KaushalTarpara <kaushaltarpara10702@gmail.com>
2023-12-09 09:45:55 +0530

    feat: Exception Handling in User API

--- src/main/java/com/kdtech/blog/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/kdtech/blog/exceptions/ResourceNotFoundException.java
@@ -11,9 +11,10 @@ public class ResourceNotFoundException extends RuntimeException{
 	String fieldName;
 	long fieldValue;
 	public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
-		super(String.format("% Not found with %s : %l",resourceName,fieldName,fieldValue));
+		super(String.format("%s Not found with %s : %s",resourceName,fieldName,fieldValue));
 		this.resourceName = resourceName;
 		this.fieldName = fieldName;
 		this.fieldValue = fieldValue;
 	}
 }
+	
